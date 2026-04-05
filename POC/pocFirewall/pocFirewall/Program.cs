3b582680df2bc71fe1d346dc9becd4de42f18ccc
ilay9006565 <ilaishalomov@gmail.com>
2026-04-06 00:18:10 +0300

    starting POC

---
+++ POC/pocFirewall/pocFirewall/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WindowsFirewallHelper;
+using WindowsFirewallHelper.FirewallRules;
+
+namespace pocFirewall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
